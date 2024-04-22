57aa7f9061d3dd54038cd5f4c9ac864e7ee3d682
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-22 15:55:23 -0300

    Mount auth router

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Enable CORS
+app.use(cors());
+
+//Route files
+const auth = require('./routes/auth.routes');
+
+//Mount routers
+app.use('/api/v1/auth', auth);
+
+//Load env vars
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
