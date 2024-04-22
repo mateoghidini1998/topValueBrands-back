ccc12ab3d97d908e175ad860200535e2885c46fe
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-22 14:06:59 -0300

    add server basic status

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+//Body parser
 app.use(express.json());
 
+//Enable CORS
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-    console.log("Server running on port", 3000)
+    console.log("Server running on port", PORT)
 })
