1dd9f5c971f8abe72c03f96c280f423e69b9cd67
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-22 15:55:33 -0300

    Create user model and hooks

---
+++ models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+    getSignedJwtToken() {
+      return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+      });
+    }
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate: async (user, options) => {
+        console.log(user)
+        if(user.password){
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
+  });
+  return User;
+};
