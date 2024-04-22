6b222e6b74d6c5eb6d45e32714a39273e28df17f
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-22 17:02:39 -0300

    Add Login and sendTokenResponse methods

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const {User} = require('../models')
+const { User } = require('../models')
 const asyncHandler = require('../middlewares/async')
 
+//@route    POST api/auth/register
+//@desc     Register a user
+//@access   Private
 exports.register = asyncHandler(async(req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
     
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ where: { email } });
 
     //Check if user exists
     if(user){
@@ -19,5 +22,55 @@ exports.register = asyncHandler(async(req, res, next) => {
         success: true,
         user
     })
-
 });
+
+//@route   POST api/auth
+//@desc    Authenticate user & get token
+//@access  public
+exports.login = asyncHandler(async(req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({ errors: [{ msg: 'Please enter all fields' }] });
+    }
+
+    //Check if user exists
+    let user = await User.findOne({ email });
+
+    if(!user){
+        return res.status(400).json({ errors: [{ msg: 'User not found' }] });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if(!isMatch){
+        return res.status(401).json({ errors: [{ msg: 'Invalid credentials' }] })
+    }
+
+    sendTokenResponse(user, 200, res);
+})
+
+//Get Token from model, create a cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+     const token = user.getSignedJwtToken();
+   
+     const options = {
+       expires: new Date(
+         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+       ),
+       httpOnly: true
+     };
+   
+     if (process.env.NODE_ENV === 'production') {
+       options.secure = true;
+     }
+   
+     res
+       .status(statusCode)
+       .cookie('token', token, options)
+       .json({
+         success: true,
+         token
+       });
+ };
+ 
