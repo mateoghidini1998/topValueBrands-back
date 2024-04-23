3626e5a7064d07f4c5d8f3ba4d3167128a1396ab
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-23 19:31:28 -0300

    add product routes

---
+++ routes/products.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middlewares/auth');
+
+const {
+    getToken,
+} = require('../controllers/products.controller');
+
+router.post('/', getToken);
+
+module.exports = router;
