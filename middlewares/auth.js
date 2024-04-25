c87a216cd24ed87a6b80a44467f06ebfe636dbbd
mateoghidini1998 <mateghidini123@gmail.com>
2024-04-24 21:02:00 -0300

    Modify error message

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -35,7 +35,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
     return (req, res, next) => {
         if(!roles.includes(req.user.role)){
-            return res.status(403).json({ msg: `User role ${req.user.role} is not authorized to access this route` });
+            return res.status(403).json({ msg: `User role "${req.user.role}" is not authorized to access this route` });
         }
     }
 }
