4b1d740ee344945f5d8b5207d78ea6d707fbca02
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-10 13:05:58 -0300

    Update port

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ dotenv.config({
     path: './.env'
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
     swaggerDoc(app, PORT);
 })
