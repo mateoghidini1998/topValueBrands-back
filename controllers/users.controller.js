7d6e1d34bb597b09e6a2c66984b3d74decbbd939
ramirosarasola <ramiro.sarasola@gmail.com>
2024-05-21 18:17:46 -0300

    add success on error responses

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,39 +7,39 @@ const asyncHandler = require('../middlewares/async');
 //@access   Private
 
 exports.getUsers = asyncHandler(async (req, res, next) => {
-    const users = await User.findAll();
-    return res.status(200).json({
-       success: true,
-       data: users
-    })
-});   
+   const users = await User.findAll();
+   return res.status(200).json({
+      success: true,
+      data: users
+   })
+});
 
 //@route    DELETE api/users/deleteUser
 //@desc     Delete user 
 //@access   Private
 
 exports.deleteUser = asyncHandler(async (req, res, next) => {
-    const loggedInUser = req.user;
-    const user = await User.findByPk(req.params.id);
-    console.log(loggedInUser)
-    if(loggedInUser.role !== 'admin') {
-       return res.status(401).json({ errors: [{ msg: 'User has no clearance to delete this user' }] });
-    }
-
-    if(!user) {
-       return res.status(404).json({ errors: [{ msg: 'User not found' }] });
-    }
-
-   if (loggedInUser.id === user.id) {
-       return res.status(401).json({ errors: [{ msg: 'User cannot delete themselves' }] });
+   const loggedInUser = req.user;
+   const user = await User.findByPk(req.params.id);
+   console.log(loggedInUser)
+   if (loggedInUser.role !== 'admin') {
+      return res.status(401).json({ success: false, errors: [{ msg: 'User has no clearance to delete this user' }] });
    }
 
-    await user.destroy();
+   if (!user) {
+      return res.status(404).json({ success: false, errors: [{ msg: 'User not found' }] });
+   }
 
-    return res.status(200).json({
-       success: true,
-       data: {}
-    })
+   if (loggedInUser.id === user.id) {
+      return res.status(400).json({ success: false, errors: [{ msg: 'User cannot delete themselves' }] });
+   }
+
+   await user.destroy();
+
+   return res.status(200).json({
+      success: true,
+      data: {}
+   })
 });
 
 
@@ -48,36 +48,36 @@ exports.deleteUser = asyncHandler(async (req, res, next) => {
 //@access   Private
 
 exports.updateUserRole = asyncHandler(async (req, res, next) => {
-    const loggedInUser = req.user;
-    console.log(loggedInUser)
-    
-    const user = await User.findByPk(req.params.id);
-   
-    console.log('Role:' , loggedInUser.role)
-    console.log(user)
-    if(loggedInUser.role !== 'admin') {
-       return res.status(401).json({ errors: [{ msg: 'User has no clearance to modify this user' }] });
-    }
-   
-    if(!user) {
-       return res.status(404).json({ errors: [{ msg: 'User not found' }] });
-    }
-    
-    const { role } = req.body;
-    
-    const validRoles = ['admin', 'user']; 
+   const loggedInUser = req.user;
+   console.log(loggedInUser)
 
-    const lowerCaseRole = role.toLowerCase();
-    if (!validRoles.includes(lowerCaseRole)) {
-       return res.status(400).json({ errors: [{ msg: 'Invalid role' }] });
-    }
-   
-    await user.update({role: lowerCaseRole})
-   
-    return res.status(200).json({
-       success: true,
-       data: user
-    })
+   const user = await User.findByPk(req.params.id);
+
+   console.log('Role:', loggedInUser.role)
+   console.log(user)
+   if (loggedInUser.role !== 'admin') {
+      return res.status(401).json({ errors: [{ msg: 'User has no clearance to modify this user' }] });
+   }
+
+   if (!user) {
+      return res.status(404).json({ errors: [{ msg: 'User not found' }] });
+   }
+
+   const { role } = req.body;
+
+   const validRoles = ['admin', 'user'];
+
+   const lowerCaseRole = role.toLowerCase();
+   if (!validRoles.includes(lowerCaseRole)) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid role' }] });
+   }
+
+   await user.update({ role: lowerCaseRole })
+
+   return res.status(200).json({
+      success: true,
+      data: user
+   })
 });
 
 //@route    PATCH api/users/update/:id -> of the user being updated
@@ -85,34 +85,34 @@ exports.updateUserRole = asyncHandler(async (req, res, next) => {
 //@access   Private
 
 exports.updateUser = asyncHandler(async (req, res, next) => {
-    const loggedInUser = req.user;
+   const loggedInUser = req.user;
    //  console.log(loggedInUser)
-    const user = await User.findByPk(req.params.id);
-    if(loggedInUser.role !== 'admin' && loggedInUser.id !== user.id) {
-       return res.status(401).json({ errors: [{ msg: 'User has no clearance to modify this user' }] });
-    }
-    if(!user) {
-       return res.status(404).json({ errors: [{ msg: 'User not found' }] });
+   const user = await User.findByPk(req.params.id);
+   if (loggedInUser.role !== 'admin' && loggedInUser.id !== user.id) {
+      return res.status(401).json({success:false, errors: [{ msg: 'User has no clearance to modify this user' }] });
    }
-   
+   if (!user) {
+      return res.status(404).json({success:false, errors: [{ msg: 'User not found' }] });
+   }
+
    // Check if user exists by email
    const { firstName, lastName, email, password, confirmPassword, role } = req.body;
    console.log(req.body);
-   
-   if(password !== confirmPassword) {
-      return res.status(400).json({ errors: [{ msg: 'Passwords do not match' }] });
+
+   if (password !== confirmPassword) {
+      return res.status(400).json({success:false, errors: [{ msg: 'Passwords do not match' }] });
    }
 
    const userExists = await User.findOne({ where: { email } });
    if (userExists && userExists.id !== user.id) {
-       return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      return res.status(400).json({success:false, errors: [{ msg: 'User already exists' }] });
    }
 
-    await user.update({firstName, lastName, email, role, password})
-    return res.status(200).json({
-       success: true,
-       data: user
-    })
+   await user.update({ firstName, lastName, email, role, password })
+   return res.status(200).json({
+      success: true,
+      data: user
+   })
 })
 
 
@@ -125,22 +125,22 @@ exports.changePassword = asyncHandler(async (req, res, next) => {
    console.log(loggedInUser)
    const user = await User.findByPk(req.params.id);
 
-   if(loggedInUser.role !== 'admin' && loggedInUser.id !== user.id) {
+   if (loggedInUser.role !== 'admin' && loggedInUser.id !== user.id) {
       return res.status(401).json({ errors: [{ msg: 'User has no clearance to modify this user' }] });
    }
 
-   if(!user) {
+   if (!user) {
       return res.status(404).json({ errors: [{ msg: 'User not found' }] });
    }
 
    const { password, repeatPassword } = req.body;
 
-   if(password !== repeatPassword) {
+   if (password !== repeatPassword) {
       return res.status(400).json({ errors: [{ msg: 'Passwords do not match' }] });
    }
 
    await user.update({ password })
-   
+
    return res.status(200).json({
       success: true,
       data: user
