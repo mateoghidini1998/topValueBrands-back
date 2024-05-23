679f53bf75dd4bfcda4c91a7559eae2d48cd4110
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-23 15:38:05 -0300

    Add dynamic date parms and delete unnecessary methods

--- utils/pogenerator.utils.js
+++ utils/pogenerator.utils.js
@@ -4,18 +4,22 @@ const asyncHandler = require('../middlewares/async')
 const path = require('path');
 const dotenv = require('dotenv');
 const zlib = require('zlib');
+const moment = require('moment');
 
 
 dotenv.config({ path: './.env' });
+let reportId = ''
 
 const createReport = asyncHandler(async (req, res, next) => {
+  const dataEndTime = moment().utc().endOf('day').toISOString();
+  const dataStartTime = moment().utc().subtract(30, 'days').startOf('day').toISOString();
   const url = `${process.env.AMZ_BASE_URL}/reports/2021-06-30/reports`;
 
   const requestBody = {
     "reportType": "GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL",
     "marketplaceIds": [`${process.env.MARKETPLACE_US_ID}`],
-    "dataStartTime": "2024-04-20T00:00:00Z",
-    "dataEndTime": "2024-05-20T23:59:59Z",
+    "dataStartTime": dataStartTime,
+    "dataEndTime": dataEndTime,
     "custom": true
   };
 
@@ -29,7 +33,6 @@ const createReport = asyncHandler(async (req, res, next) => {
   return response.data.reportId;
 });
 
-let reportId = ''
 const pollReportStatus = async (reportId, accessToken) => {
   const url = `${process.env.AMZ_BASE_URL}/reports/2021-06-30/reports/${reportId}`;
   let reportStatus = '';
@@ -49,7 +52,7 @@ const pollReportStatus = async (reportId, accessToken) => {
   return reportStatus;
 };
 
-const getReportById = asyncHandler(async (req, res, next) => {
+exports.getReportById = asyncHandler(async (req, res, next) => {
   // Call createReport and get the reportId
   const reportId = await createReport(req, res, next);
   const accessToken = req.headers['x-amz-access-token'];
@@ -75,56 +78,8 @@ const getReportById = asyncHandler(async (req, res, next) => {
   }
 });
 
-exports.generateReport = asyncHandler(async (req, res, next) => {
-  const reportData = await getReportById(req, res, next);
 
-  if (!reportData || !reportData.reportDocumentId) {
-    throw new Error('Report data is invalid or missing reportDocumentId');
-  }
-
-  const documentId = reportData.reportDocumentId;
-
-  const response = await axios.get(`${process.env.AMZ_BASE_URL}/reports/2021-06-30/documents/${documentId}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-amz-access-token': req.headers['x-amz-access-token']
-    }
-  });
-
-  if (!response.data || !response.data.url) {
-    throw new Error('Failed to retrieve document URL from response');
-  }
-
-  const documentUrl = response.data.url;
-  const compressionAlgorithm = response.data.compressionAlgorithm;
-
-  // Obtener el contenido del documento desde la URL
-  const documentResponse = await axios.get(documentUrl, { responseType: 'arraybuffer' });
-
-  // Descomprimir y decodificar los datos si es necesario
-  let decodedData;
-  if (compressionAlgorithm === 'GZIP') {
-    decodedData = zlib.gunzipSync(Buffer.from(documentResponse.data));
-  } else {
-    decodedData = Buffer.from(documentResponse.data);
-  }
-
-  // Convertir los datos decodificados a string
-  const dataString = decodedData.toString('utf-8');
-
-  // Verificar que dataString no sea nulo ni indefinido antes de devolverlo
-  if (!dataString) {
-    throw new Error('Failed to decode report data');
-  }
-
-  const jsonData = parseReportToJSON(dataString);
-
-  return res.json(jsonData);
-});
-
-
-
-const parseReportToJSON = (dataString) => {
+exports.parseReportToJSON = (dataString) => {
   const results = [];
   const lines = dataString.split('\n');
   const keys = lines[0].split('\t');
