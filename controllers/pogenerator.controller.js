321ce2751ed26dd559bf65bc5ae05a8df3a19527
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-23 15:38:37 -0300

    Create methods to retrieve saleranks from keepa api in groups of 10 asins

--- controllers/pogenerator.controller.js
+++ controllers/pogenerator.controller.js
@@ -6,34 +6,41 @@ const axios = require('axios')
 dotenv.config({ path: './.env' });
 
 exports.getProductsTrackedData = asyncHandler(async (req, res, next) => {
-    const products = await Product.findAll();
-    
-    const asins = products.slice(0, 1).map(product => product.ASIN);
+    try {
+        // Obtén solo 10 productos
+        const products = await Product.findAll({ limit: 10 });
+        console.log('Fetched products:', products);
 
-    const keepaDataPromises = asins.map(getKeepaData);
-    const keepaDataResults = await Promise.all(keepaDataPromises);
+        // Crea un solo grupo de 10 ASINs
+        const asinGroup = products.map(product => product.ASIN).join(',');
+        console.log('ASIN group:', asinGroup);
 
-    const validKeepaData = keepaDataResults.filter(data => data !== null)
+        // Realiza la solicitud a la API de Keepa para el grupo de ASINs
+        const keepaDataResponse = await getKeepaData(asinGroup);
+        console.log('Keepa data response:', keepaDataResponse);
 
-    return res.status(200).json({ 
-        success: true,
-        suggestedLowerPrice: validKeepaData[0].products[0].suggestedLowerPrice,
-        salesRanks: validKeepaData[0].products[0].salesRanks,
-        salesRankReferenceHistory: validKeepaData[0].products[0].salesRankReferenceHistory
-    })
-
+        // Procesa la respuesta según sea necesario
+        return res.status(200).json({ success: true, data: keepaDataResponse });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return res.status(500).json({ success: false, message: 'Error fetching data from Keepa.' });
+    }
 });
 
-const getKeepaData = async (asin) => {
-    const apiKey = process.env.KEEPA_API_KEY
-
-    const url = `https://api.keepa.com/product?key=${apiKey}&domain=1&asin=${asin}&stats=1`
-
+const getKeepaData = async (asinGroup) => {
+    const apiKey = process.env.KEEPA_API_KEY;
+    const url = `https://api.keepa.com/product?key=${apiKey}&domain=1&asin=${asinGroup}&stats=1`;
+    console.log(url)
     try {
+        console.log('Fetching data from Keepa for ASIN group:', asinGroup);
         const response = await axios.get(url);
+        console.log('Response from Keepa:', response.data);
+        if (!response.data) {
+            throw new Error('No data received from Keepa');
+        }
         return response.data;
     } catch (error) {
-        console.error(`Error fetching data for ASIN ${asin}: `, error);
-        return null;
+        console.error('Error fetching data for ASIN group:', asinGroup, error.response ? error.response.data : error.message);
+        throw error;
     }
-}
+};
