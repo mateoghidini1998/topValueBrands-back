b6c5b1ad682b332dcba74e5aa1fef73b73ab5130
mateoghidini1998 <mateghidini123@gmail.com>
2024-06-03 16:26:06 -0300

    Create purchase orders and purchase orders products migrations and models

---
+++ migrations/20240603182421-create-purchase-order.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PurchaseOrders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_number: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      supplier_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Suppliers',
+          key: 'id'
+        }
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      total_price: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PurchaseOrders');
+  }
+};
---
+++ models/purchaseorder.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PurchaseOrder extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      PurchaseOrder.belongsTo(models.Supplier, { foreignKey: 'supplier_id', as: 'suppliers' });
+      PurchaseOrder.hasMany(models.PurchaseOrderProduct, { foreignKey: 'purchase_order_id', as: 'purchaseOrderProducts' });
+    }
+  }
+  PurchaseOrder.init({
+    order_number: DataTypes.STRING,
+    supplier_id: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    total_price: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'PurchaseOrder',
+  });
+  return PurchaseOrder;
+};
