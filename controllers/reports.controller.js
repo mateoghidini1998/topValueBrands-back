25f2f2c417ebf09f57fcae0ec16287cefc585a3e
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-02 17:59:34 -0300

    improve sync database algorithm performance

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -1,3 +1,6 @@
+const { Sequelize, Op } = require('sequelize');
+const { sequelize } = require('../models');
+
 const asyncHandler = require('../middlewares/async');
 const { Product } = require('../models');
 const { sendCSVasJSON } = require('../utils/utils');
@@ -31,30 +34,102 @@ exports.syncDBWithAmazon = asyncHandler(async (req, res, next) => {
   }
 });
 
+// const processReport = async (productsArray) => {
+//   try {
+//     const newProducts = [];
+//     const updatedProducts = [];
+
+//     //Check if there is any difference between our products and the productsArray passed by props.
+//     const allOurProducts = await Product.findAll();
+
+
+//     for (const product of productsArray) {
+//       const [result, created] = await Product.upsert({
+//         ASIN: product.asin,
+//         product_name: product['product-name'],
+//         seller_sku: product.sku,
+//         in_seller_account: true,
+//         FBA_available_inventory: parseFloat(
+//           product['afn-fulfillable-quantity']
+//         ),
+//         reserved_quantity: parseFloat(product['afn-reserved-quantity']),
+//         Inbound_to_FBA: parseFloat(product['afn-inbound-shipped-quantity']),
+//       });
+
+//       if (created) {
+//         newProducts.push(product);
+//       } else {
+//         updatedProducts.push(product);
+//       }
+//     }
+
+//     return {
+//       newSyncProductsQuantity: newProducts.length,
+//       newSyncQuantity: updatedProducts.length,
+//       newSyncProducts: newProducts,
+//       newSyncData: updatedProducts,
+//     };
+//   } catch (error) {
+//     console.error('Error al actualizar o crear productos:', error);
+//     throw error;
+//   }
+// };
+
 const processReport = async (productsArray) => {
+  const t = await sequelize.transaction();
   try {
     const newProducts = [];
     const updatedProducts = [];
 
-    for (const product of productsArray) {
-      const [result, created] = await Product.upsert({
-        ASIN: product.asin,
-        product_name: product['product-name'],
-        seller_sku: product.sku,
-        in_seller_account: true,
-        FBA_available_inventory: parseFloat(
-          product['afn-fulfillable-quantity']
-        ),
-        reserved_quantity: parseFloat(product['afn-reserved-quantity']),
-        Inbound_to_FBA: parseFloat(product['afn-inbound-shipped-quantity']),
-      });
+    // Obtener todos nuestros productos de la base de datos
+    const allOurProducts = await Product.findAll({ transaction: t });
+    const ourProductsMap = new Map();
 
-      if (created) {
-        newProducts.push(product);
+    for (const product of allOurProducts) {
+      ourProductsMap.set(product.ASIN, product);
+    }
+
+    for (const product of productsArray) {
+      const existingProduct = ourProductsMap.get(product.asin);
+
+      if (existingProduct) {
+        let needsUpdate = false;
+
+        // Comparar los valores
+        if (existingProduct.FBA_available_inventory !== parseFloat(product['afn-fulfillable-quantity'])) {
+          needsUpdate = true;
+          existingProduct.FBA_available_inventory = parseFloat(product['afn-fulfillable-quantity']);
+        }
+        if (existingProduct.reserved_quantity !== parseFloat(product['afn-reserved-quantity'])) {
+          needsUpdate = true;
+          existingProduct.reserved_quantity = parseFloat(product['afn-reserved-quantity']);
+        }
+        if (existingProduct.Inbound_to_FBA !== parseFloat(product['afn-inbound-shipped-quantity'])) {
+          needsUpdate = true;
+          existingProduct.Inbound_to_FBA = parseFloat(product['afn-inbound-shipped-quantity']);
+        }
+
+        if (needsUpdate) {
+          await existingProduct.save({ transaction: t });
+          updatedProducts.push(existingProduct);
+        }
       } else {
-        updatedProducts.push(product);
+        const newProduct = await Product.create({
+          ASIN: product.asin,
+          product_name: product['product-name'],
+          seller_sku: product.sku,
+          in_seller_account: true,
+          FBA_available_inventory: parseFloat(product['afn-fulfillable-quantity']),
+          reserved_quantity: parseFloat(product['afn-reserved-quantity']),
+          Inbound_to_FBA: parseFloat(product['afn-inbound-shipped-quantity']),
+        }, { transaction: t });
+
+        newProducts.push(newProduct);
+
       }
     }
+
+    await t.commit();
 
     return {
       newSyncProductsQuantity: newProducts.length,
@@ -63,6 +138,7 @@ const processReport = async (productsArray) => {
       newSyncData: updatedProducts,
     };
   } catch (error) {
+    await t.rollback();
     console.error('Error al actualizar o crear productos:', error);
     throw error;
   }
