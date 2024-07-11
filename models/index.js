ef128078839e8dcfa8ab13e5c7547e7271d95475
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-11 09:47:11 -0300

    add env config to db configuration

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './.env',
+});
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
