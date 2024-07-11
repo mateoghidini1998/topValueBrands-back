5701da71937e4e9caacd2cfa3728db0470a7a65b
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-11 15:57:52 -0300

    implement logs with winston

---
+++ logger/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'logger/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logger/combined.log' }),
+  ],
+});
+
+
+
+module.exports = logger
