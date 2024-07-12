bb432663df229160d51b9e8b2f762c9e9f0b30fd
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-12 16:26:54 -0300

    Update redis client

--- redis/redis.js
+++ redis/redis.js
@@ -10,36 +10,37 @@ const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
 if (!cacheHostName) throw new Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
 if (!cachePassword) throw new Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
 
-const redisClient = redis.createClient({
+/* const redisClient = redis.createClient({
   // redis for TLS
   url: `redis://default:${cachePassword}@${cacheHostName}:6380`,
   tls: {
     rejectUnauthorized: false
   }
-});
+}); */
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
+exports.connect = async () => {
+  const client = redis.createClient({
+      url: 'redis://default:${cachePassword}@${cacheHostName}:6380',
+      tls: {
+        rejectUnauthorized: false
+      }
+  });
 
-redisClient.on('error', (err) => {
-  console.error('Redis error:', err);
-});
+  client.on('connect', () => {
+      console.log('Redis client connected to the server');
+  });
 
-const connectRedis = async () => {
-  try {
-    await redisClient.connect();
-    console.log('Redis client connected');
+  client.on('error', (err) => {
+      console.log('Redis client not connected to the server: ' + err);
+  });
 
-    // Example: Set and get a value
-    await redisClient.set('key', 'value');
-    const value = await redisClient.get('key');
-    console.log(value); // Should print 'value'
-  } catch (err) {
-    console.error('Failed to connect to Redis:', err);
-  }
-};
+  client.on('end', () => {
+      console.log('Redis client connection closed');
+  });
 
-connectRedis();
+  await client.connect();
 
-module.exports = redisClient;
+  return client;
+}
+
+
