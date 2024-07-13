7437ee485f4b899293e25478a17a0979840e9af2
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-13 20:05:19 -0300

    Revert "update redis client"
    This reverts commit d964bec631eaff91ed08014d0acc2aa1020f1337.

--- redis/redis.js
+++ redis/redis.js
@@ -3,45 +3,53 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
 
-// Variables de entorno para la caché
+// Environment variables for cache
 const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
 const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
 
 if (!cacheHostName) throw new Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
 if (!cachePassword) throw new Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
 
+/* const redisClient = redis.createClient({
+  // redis for TLS
+  url: `redis://default:${cachePassword}@${cacheHostName}:6379`,
+  tls: {
+    rejectUnauthorized: false
+  }
+}); */
+
 exports.connect = async () => {
   let client;
 
   if (process.env.NODE_ENV === 'production') {
     client = redis.createClient({
-      // Configuración de Redis para TLS en producción
-      url: `redis://${cachePassword}@${cacheHostName}:6380`,
+      // Redis for TLS in production
+      url: `redis://default:${cachePassword}@${cacheHostName}:6380`,
       tls: {
-        rejectUnauthorized: true // Asegúrate de que este valor sea verdadero para requerir SSL
+        rejectUnauthorized: false
       }
     });
   } else {
-    // Configuración local de Redis para pruebas
     client = redis.createClient({
+      // Local Redis configuration
       host: 'localhost',
       port: 6379
     });
   }
 
   client.on('connect', () => {
-    console.log('Cliente Redis conectado al servidor');
+    console.log('Redis client connected to the server');
   });
 
   client.on('error', (err) => {
-    console.error('El cliente Redis no pudo conectarse al servidor:', err);
+    console.log('Redis client not connected to the server: ' + err);
   });
 
   client.on('end', () => {
-    console.log('La conexión del cliente Redis se cerró');
+    console.log('Redis client connection closed');
   });
 
   await client.connect();
 
   return client;
-};
+}
