562a8194023a9c0063574bc0e3f0e8a75827b430
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-14 11:29:52 -0300

    Add cronjob for generating report

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ const purchaseorders = require('./routes/purchaseorders.routes');
 
 const { swaggerDoc } = require('./routes/swagger.routes');
 const cron = require('node-cron');
-const { fetchNewToken } = require('./middlewares/lwa_token');
+const { addAccessTokenHeader } = require('./middlewares/lwa_token');
+const { syncDBWithAmazon } = require('./controllers/reports.controller')
 
 //Mount routers
 app.use('/api/v1/auth', auth);
@@ -47,27 +48,26 @@ app.listen(5000, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
   swaggerDoc(app, PORT);
 
-  cron.schedule('* * * 5 *', async () => {
-    console.log('running a task every day at 5am');
-    // try {
-    //   const accessToken = fetchNewToken();
-    //   // Call createReport and get the reportId
-    //   const report = await sendCSVasJSON(req, res, next);
-
-    //   // Continue with the rest of the code after sendCSVasJSON has completed
-    //   const newSync = await processReport(report);
-
-    //   // Call addImageToProducts to add images to new products
-    //   // const newProducts = await Product.findAll({ where: { product_image: null } || { product_image: '' } });
-    //   // const accessToken = req.headers['x-amz-access-token'];
-    //   // const imageSyncResult = await addImageToProducts(newProducts, accessToken);
-    //   const imageSyncResult = await addImageToNewProducts(accessToken);
-
-    //   res.json({ newSync, imageSyncResult });
-    //   return { newSync, imageSyncResult };
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-  });
+  cron.schedule('28 11 * * *', async () => {
+    console.log('running a task every day at 11:26am');
+      try {
+        console.log('Estoy aca')
+        const req = { headers: {} }; // Mock request object with headers
+        const res = {
+          json: (data) => console.log('Sync result:', data),
+        }; // Mock response object
+        const next = (error) => {
+          if (error) {
+            console.error('Error during sync:', error);
+          }
+        }; // Mock next function for error handling
+        console.log('Por ejecutar token')
+        await addAccessTokenHeader(req, res, async () => {
+          console.log('Por generar report')
+          await syncDBWithAmazon(req, res, next);
+        });
+      } catch (error) {
+        console.error('Error during scheduled sync:', error);
+      }
+    });
 });
