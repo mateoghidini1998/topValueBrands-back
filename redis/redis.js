5563915853857facf2bdfcbcd6f953ad5c6986d3
mateoghidini1998 <mateghidini123@gmail.com>
2024-07-15 14:08:28 -0300

    Implement redis client

--- redis/redis.js
+++ redis/redis.js
@@ -10,30 +10,25 @@ const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
 if (!cacheHostName) throw new Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
 if (!cachePassword) throw new Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
 
-/* const redisClient = redis.createClient({
-  // redis for TLS
-  url: `redis://default:${cachePassword}@${cacheHostName}:6379`,
-  tls: {
-    rejectUnauthorized: false
-  }
-}); */
 
 exports.connect = async () => {
   let client;
 
   if (process.env.NODE_ENV === 'production') {
     client = redis.createClient({
-      // Redis for TLS in production
-      url: `redis://default:${cachePassword}@${cacheHostName}:6380`,
-      tls: {
-        rejectUnauthorized: false
+      url: `redis://${cachePassword}@${cacheHostName}:6380`,
+      socket: {
+        tls: {
+          rejectUnauthorized: true
+        }
       }
     });
   } else {
     client = redis.createClient({
-      // Local Redis configuration
-      host: 'localhost',
-      port: 6379
+      socket: {
+        host: 'localhost',
+        port: 6379
+      }
     });
   }
 
