0b467537d084efb9b45814488ae55bf0475dcdbc
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-18 13:08:25 -0300

    fix trackedproducts to get the estimate fees

--- controllers/trackedproducts.controller.js
+++ controllers/trackedproducts.controller.js
@@ -81,105 +81,11 @@ exports.getTrackedProducts = asyncHandler(async (req, res, next) => {
     });
   }
 });
-//@route GET api/v1/pogenerator/trackedproducts
-//@desc  Track products and save them into db from keepa api data and order reports from AMZ API.
-//@access Private
-// exports.generateTrackedProductsData = asyncHandler(async (req, res, next) => {
-//   logger.info('Start generateTrackedProductsData');
 
-//   try {
-//     const products = await fetchProducts({ limit: LIMIT_PRODUCTS });
-//     logger.info('Fetched products successfully');
-
-//     const [orderData, keepaData] = await Promise.all([
-//       saveOrders(req, res, next, products),
-//       getProductsTrackedData(products),
-//     ]);
-//     logger.info('Fetched order data and keepa data successfully');
-
-//     const combinedData = keepaData.map((keepaItem) => {
-//       const orderItem = orderData.find((o) => o.product_id === keepaItem.product_id) || {};
-//       const unitsSold = orderItem.quantity || 0;
-//       const productVelocity = orderItem.velocity || 0;
-//       const lowestFbaPriceInDollars = keepaItem.lowestFbaPrice ? keepaItem.lowestFbaPrice / 100 : null;
-
-//       return {
-//         product_id: keepaItem.product_id,
-//         current_rank: keepaItem.currentSalesRank || null,
-//         thirty_days_rank: keepaItem.avg30 || null,
-//         ninety_days_rank: keepaItem.avg90 || null,
-//         units_sold: unitsSold,
-//         product_velocity: productVelocity,
-//         lowest_fba_price: lowestFbaPriceInDollars,
-//       };
-//     });
-
-//     await TrackedProduct.bulkCreate(combinedData, {
-//       updateOnDuplicate: [
-//         'current_rank',
-//         'thirty_days_rank',
-//         'ninety_days_rank',
-//         'units_sold',
-//         'product_velocity',
-//         'lowest_fba_price',
-//       ],
-//     });
-//     logger.info('Combined data bulk created successfully');
-
-//     const feeEstimates = await getEstimateFees(req, res, next, products);
-//     console.log(`feeEstimates cantidad registros: ${feeEstimates.length}`);
-//     console.log(`feeEstimates registro ejemplo 1: ${feeEstimates[0]}`);
-//     logger.info('Fetched fee estimates successfully');
-
-//     const productCosts = await Product.findAll({
-//       where: {
-//         id: products.map((product) => product.id),
-//       },
-//       attributes: ['id', 'product_cost'],
-//     });
-//     logger.info('Fetched product costs successfully');
-
-//     const costMap = productCosts.reduce((acc, product) => {
-//       acc[product.id] = product.product_cost;
-//       return acc;
-//     }, {});
-
-//     const updatePromises = feeEstimates.map((feeEstimate) => {
-//       const productCost = costMap[feeEstimate.product_id] || 0;
-//       const lowestFbaPrice = combinedData.find((item) => item.product_id === feeEstimate.product_id)?.lowest_fba_price || 0;
-//       const fees = feeEstimate.fees || 0;
-//       const profit = lowestFbaPrice - fees - productCost;
-
-//       return TrackedProduct.update(
-//         {
-//           fees: fees,
-//           profit: profit,
-//         },
-//         { where: { product_id: feeEstimate.product_id } }
-//       );
-//     });
-
-//     await Promise.all(updatePromises);
-//     logger.info('Batch update of fees and profit completed successfully');
-
-//     res.status(200).json({
-//       message: 'Data combined and saved successfully.',
-//       data: combinedData,
-//     });
-//     logger.info('Response sent successfully');
-//   } catch (error) {
-//     logger.error('Error combining and saving data', { error: error.message });
-//     // res.status(500).json({
-//     //   success: false,
-//     //   message: 'Error combining and saving data.',
-//     // });
-//   }
-// });
+const LIMIT_PRODUCTS = 20000; // Límite de productos para fetch
+const OFFSET_PRODUCTS = 0; // Límite de productos para fetch
 
 const BATCH_SIZE = 20; // Tamaño del batch para la segunda etapa
-const LIMIT_PRODUCTS = 250; // Límite de productos para fetch
-const OFFSET_PRODUCTS = 335; // Límite de productos para fetch
-const MAX_REQUESTS_BEFORE_DELAY = 20; // Límite de solicitudes antes de aplicar un delay
 const DELAY_TIME_MS = 30000; // Tiempo de delay en milisegundos
 
 exports.generateTrackedProductsData = asyncHandler(async (req, res, next) => {
@@ -229,20 +135,38 @@ exports.generateTrackedProductsData = asyncHandler(async (req, res, next) => {
     });
     logger.info('Combined data saved successfully');
 
+    // Obtener los productos que están relacionados con los TrackedProducts
+    const trackedProductIds = combinedData.map(item => item.product_id);
+
+    const relatedProducts = await Product.findAll({
+      where: {
+        id: trackedProductIds,
+      },
+    });
+    logger.info('Fetched related products successfully');
+
+
+    // Encontrar y registrar los productos no registrados como TrackedProducts
+    const allProductIds = products.map(product => product.id);
+    const untrackedProductIds = allProductIds.filter(id => !trackedProductIds.includes(id));
+
+    if (untrackedProductIds.length > 0) {
+      logger.warn(`Products not tracked: ${untrackedProductIds.join(', ')}`);
+    }
+
+
     // Segunda etapa: Obtener las estimaciones de tarifas y actualizar la información de los productos usando BATCH_SIZE
-    for (let i = 0; i < products.length; i += BATCH_SIZE) {
-      const productBatch = products.slice(i, i + BATCH_SIZE);
+    for (let i = 0; i < relatedProducts.length; i += BATCH_SIZE) {
+      const productBatch = relatedProducts.slice(i, i + BATCH_SIZE);
       console.log('fetching get estimate fees for batch for' + productBatch.length);
 
-      console.log('waiting 1 min')
-      // await delay(DELAY_TIME_MS_ESTIMATE_FEES);
+      console.log('waiting 1 min');
       await new Promise((resolve) => setTimeout(resolve, DELAY_TIME_MS));
-      console.log('finished waiting 1min')
+      console.log('finished waiting 1min');
 
       const feeEstimates = await getEstimateFees(req, res, next, productBatch).catch((error) => {
         logger.error(`getEstimateFees failed for batch ${i / BATCH_SIZE + 1}: ${error.message}`);
         console.log(`getEstimateFees failed for batch ${i / BATCH_SIZE + 1}: ${error.message}`);
-        // throw new Error(`getEstimateFees failed for batch ${i / BATCH_SIZE + 1}: ${error.message}`);
       });
       logger.info(`Fetched fee estimates for batch ${i / BATCH_SIZE + 1} successfully`);
 
@@ -272,9 +196,6 @@ exports.generateTrackedProductsData = asyncHandler(async (req, res, next) => {
           fees: fees,
           profit: profit,
         };
-
-
-
       });
 
       await TrackedProduct.bulkCreate(finalData, {
@@ -465,81 +386,6 @@ const getEstimateFees = async (req, res, next, products) => {
 
   return feeEstimate;
 };
-
-
-// const getEstimateFees = async (req, res, next, products) => {
-//   console.log("Executing getEstimateFees... for products: ", products.length + " products")
-//   logger.info("Executing getEstimateFees... for products: ", products.length + " products")
-
-//   const feeEstimate = [];
-//   let requestCount = 0;
-
-//   for (const product of products) {
-//     console.log(`Executing getEstimateFees... for product ${product.ASIN}...`);
-//     logger.info(`Executing getEstimateFees... for product ${product.ASIN}...`);
-//     const url = `https://sellingpartnerapi-na.amazon.com/products/fees/v0/items/${product.ASIN}/feesEstimate`;
-//     const trackedProduct = await TrackedProduct.findOne({
-//       where: { product_id: product.id },
-//     });
-
-//     if (!trackedProduct) {
-//       throw new Error(
-//         `TrackedProduct not found for product id ${product.id}`
-//       );
-//     }
-
-//     const body = {
-//       FeesEstimateRequest: {
-//         MarketplaceId: 'ATVPDKIKX0DER',
-//         IsAmazonFulfilled: true,
-//         Identifier: product.ASIN,
-//         PriceToEstimateFees: {
-//           ListingPrice: {
-//             Amount: trackedProduct.lowest_fba_price.toString(),
-//             CurrencyCode: 'USD',
-//           },
-//         },
-//       },
-//     };
-
-//     if (requestCount > 0 && requestCount % MAX_REQUESTS_BEFORE_DELAY_ESTIMATE_FEES === 0) {
-//       await delay(DELAY_TIME_MS_ESTIMATE_FEES);
-//     }
-
-//     const response = await axios.post(url, body, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'x-amz-access-token': req.headers['x-amz-access-token'],
-//       },
-//     });
-
-//     requestCount++;
-
-//     if (!response.data || !response.data.FeesEstimateResult) {
-//       throw new Error(`Failed to retrieve fee estimate for ${product.ASIN}`);
-//     }
-
-//     const feesEstimateResult = response.data.FeesEstimateResult;
-
-//     if (
-//       !feesEstimateResult ||
-//       !feesEstimateResult.FeesEstimateIdentifier ||
-//       !feesEstimateResult.FeesEstimate
-//     ) {
-//       throw new Error(
-//         `Invalid fee estimate result for product id ${product.id}`
-//       );
-//     }
-
-//     const feesEstimate = feesEstimateResult.FeesEstimate.TotalFeesEstimate.Amount;
-//     feeEstimate.push({
-//       product_id: product.id,
-//       fees: feesEstimate,
-//     });
-//   }
-//   return feeEstimate;
-// };
-
 
 exports.getEstimateFees = asyncHandler(async (req, res, next) => {
 
