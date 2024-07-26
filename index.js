1d22da427aa19e41f213b3f96de9696a6fd68c87
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-25 23:02:55 -0300

    change the cronjobs hour to 2:15 am

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.listen(PORT, () => {
   swaggerDoc(app, PORT);
 
   // cron.schedule('00 */4 * * *', async () => {
-  cron.schedule('22 10 * * *', async () => {
+  cron.schedule('15 02 * * *', async () => {
     logger.info('Cron executed at ' + new Date().toLocaleString());
 
     // Mock request, response, and next for the cron job context
