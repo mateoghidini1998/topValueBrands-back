813b19d19c123d3d0cd7a36086f77b397df46df0
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-26 10:39:47 -0300

    update cronjobs hour

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.listen(PORT, () => {
   swaggerDoc(app, PORT);
 
   // cron.schedule('00 */4 * * *', async () => {
-  cron.schedule('15 02 * * *', async () => {
+  cron.schedule('50 13 * * *', async () => {
     logger.info('Cron executed at ' + new Date().toLocaleString());
 
     // Mock request, response, and next for the cron job context
