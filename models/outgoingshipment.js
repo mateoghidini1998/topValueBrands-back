096c180f139cc9f45923cab757b08382729ff009
ramiro <ramiro.sarasola@gmail.com>
2024-09-09 18:07:45 -0300

    create outgoingshipment table

---
+++ models/outgoingshipment.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class OutgoingShipment extends Model {
+    static associate(models) {
+      OutgoingShipment.belongsToMany(models.PurchaseOrderProduct, {
+        through: models.OutgoingShipmentProduct,
+        foreignKey: 'outgoing_shipment_id',
+        otherKey: 'purchase_order_product_id',
+      });
+    }
+
+  }
+
+  OutgoingShipment.init(
+    {
+      shipment_number: {
+        type: DataTypes.STRING,
+        allowNull: false, // Requerido; ajusta según tus necesidades
+      },
+    },
+    {
+      sequelize,
+      modelName: 'OutgoingShipment',
+    }
+  );
+
+  return OutgoingShipment;
+};
