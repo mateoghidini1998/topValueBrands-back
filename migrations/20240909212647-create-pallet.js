f9de75fd77eb5e3f1d29191e00938df0aa732818
ramiro <ramiro.sarasola@gmail.com>
2024-09-09 20:18:43 -0300

    create pallet table

---
+++ migrations/20240909212647-create-pallet.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pallets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      pallet_number: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      warehouse_location_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'warehouselocations',
+          key: 'id'
+        }
+      },
+      purchase_order_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'purchaseorders',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addIndex('pallets', ['warehouse_location_id']);
+    await queryInterface.addIndex('pallets', ['purchase_order_id']);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex('pallets', ['warehouse_location_id']);
+    await queryInterface.removeIndex('pallets', ['purchase_order_id']);
+
+    await queryInterface.dropTable('Pallets');
+  }
+};
---
+++ models/pallet.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pallet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Pallet.belongsTo(models.WarehouseLocation, {
+        foreignKey: 'warehouse_location_id',
+        as: 'warehouseLocation'
+      })
+      Pallet.belongsTo(models.PurchaseOrder, {
+        foreignKey: 'purchase_order_id',
+        as: 'purchaseOrder'
+      })
+    }
+  }
+  Pallet.init({
+    pallet_number: DataTypes.STRING,
+    warehouse_location_id: DataTypes.INTEGER,
+    purchase_order_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Pallet',
+  });
+  return Pallet;
+};
